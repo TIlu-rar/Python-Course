dabc1fc2a5186a945854e387ea6751f9b431b76c
Iliyas Turar <Turar.01.26@gmail.com>
2024-11-21 17:20:34 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+x = int(input("Бірінші сан: "))
+y = int(input("Екінші сан: "))
+z = input("Таңба: ")
+
+if z == '+':
+    print(x + y)
+if z == '-':
+    print(x - y)
+if z == '*':
+    print(x * y)
+if z == '/':
+    print(x / y)
