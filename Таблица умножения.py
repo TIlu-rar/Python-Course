f43835b7e354d1d0660cf0ff41aa8a633d81aba1
Iliyas Turar <Turar.01.26@gmail.com>
2024-11-27 17:32:09 +0500

    Add files via upload

---
+++ Таблица умножения.py
@@ -0,0 +1,21 @@
+for x in range(1, 11):
+    for y in range(1, 11):
+        print(f"{x*y:^5}", end="")
+    print("")
+
+
+
+
+
+
+
+
+# while x <= 10:
+#     y = 1
+#     while y <= 10:
+#         print(x, "*", y, "=", x*y)
+#         y = y + 1
+#     print("")
+#     if x == z:
+#         break
+#     x = x + 1
